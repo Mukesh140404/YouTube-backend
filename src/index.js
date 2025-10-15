bb02c998e354a89ce5f00aa775c28fe0334055a9
devilking <devilking14041404@gmail.com>
2025-10-16 01:51:29 +0530

    express add

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((error)=>{
+    console.error("MongoDB connect failed !!!",error)
+})
 /*
 import express from 'express'
 const app = express()
